57dbb225b3ac7907beff7edc8126718e089daf38
Arina <arinameder123@gmail.com>
2022-07-07 16:08:48 +0700

    mod design items

--- src/components/DesignItem/DesignItem.vue
+++ src/components/DesignItem/DesignItem.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="design__item">
-        <div class="design__info">
+        <div class="design__info" :class="{ 'borderbot' : showMore }">
+            <p v-if="variant.title === 'pro'" class="pro">recommend</p>
             <p class="design__title">{{ variant.title }}</p>
             <p class="design__subtitle">{{ variant.subtitle }}</p>
         </div>
         <div v-if="showMore" class="design__more">
             <p v-for="item in variant.services" :key="item">{{ item }}</p>
         </div>
-        <button-app :style="btnStyle" @click="showModal">Заказать</button-app>
+        <button-app :style="btnStyle" @click="showModal"
+            @mouseenter="hover"
+            @mouseleave="hover">
+            Заказать
+            <img v-if="!onHover" class="arrow" src="@/assets/arrow-up-right.svg" alt="arrow">
+            <img v-else class="arrow" src="@/assets/arrow-up-right-white.svg" alt="arrow">
+        </button-app>
     </div>
 </template>
 
@@ -27,17 +34,26 @@
                 type: Boolean
             }
         },
+        data () {
+          return {
+              onHover: false
+          }
+        },
         computed: {
             btnStyle () {
                 return {
                     'outline': 'none',
-                    'width': '101%'
+                    'width': '101%',
+                    'align-self': 'center'
                 }
             }
         },
         methods: {
             showModal () {
                 this.$emit('showModal')
+            },
+            hover () {
+                this.onHover = !this.onHover
             }
         }
     }
